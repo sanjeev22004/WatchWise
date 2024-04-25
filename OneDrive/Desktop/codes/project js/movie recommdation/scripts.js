5a7e3119f7ac08a768e3535452a702dc38f42115
sanjeev prasad <sanjeevprasad2004@gmail.com>
2024-04-25 13:02:12 +0530

    update search section

---
+++ OneDrive/Desktop/codes/project js/movie recommdation/scripts.js
@@ -0,0 +1,37 @@
+
+const url=" https://api.tvmaze.com/search/shows?q="
+const form=document.getElementById("search_bar");
+const grid=document.getElementById("movieGrid");
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    let search_text=document.getElementById("searchBox").value;
+    
+   remove();
+
+    console.log("search_text");
+    show_movie(search_text);
+    
+})
+function remove() {
+   
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+}
+
+
+function show_movie(m){
+    axios.get(`${url}${m}`)
+    .then((res)=>{
+        for(let i of res.data){
+            if(i.show.image != null){
+            let image=document.createElement("img");
+            image.setAttribute("src",i.show.image.medium);
+            grid.append(image);
+            }
+        }
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+};
